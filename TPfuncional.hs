cd64112151e4494f6ca972f4ce7a035f8915eb57
Maximiliano Feldman <maxifeldman@hotmail.com>
2018-04-28 12:13:04 -0300

    Modificacion de Microprocesador, Instruccion nop

--- TPfuncional.hs
+++ TPfuncional.hs
@@ -1,9 +1,20 @@
-type Memoria = [(Int, Int)]
+type Posicion = Int
+type Dato = Int
 
 data Microprocesador = Microprocesador {
-  posiciones :: [Int],
+  memoria :: [(Posicion, Dato)],
   acumuladorA :: Int,
   acumuladorB :: Int,
   programCounter :: Int,
   mensajeError :: String
+} deriving Show
+
+xt8088 = Microprocesador {
+  memoria = [],
+  acumuladorA = 0,
+  acumuladorB = 0,
+  programCounter = 0,
+  mensajeError = ""
 }
+
+nop microprocesador = microprocesador{programCounter = (programCounter microprocesador) + 1}
