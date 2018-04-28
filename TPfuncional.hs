c1d2d7928cbbde1b331754e594d9d1ad270e4c26
Maximiliano Feldman <maxifeldman@hotmail.com>
2018-04-28 14:15:29 -0300

    Modificacion de Microprocesador y nop, instruccion str

--- TPfuncional.hs
+++ TPfuncional.hs
@@ -1,20 +1,29 @@
-type Posicion = Int
-type Dato = Int
-
 data Microprocesador = Microprocesador {
-  memoria :: [(Posicion, Dato)],
+  memoria :: [Int],
   acumuladorA :: Int,
   acumuladorB :: Int,
   programCounter :: Int,
   mensajeError :: String
 } deriving Show
 
+creacionMemoriaVacia :: [Int]
+creacionMemoriaVacia = replicate 1024 0
+
 xt8088 = Microprocesador {
-  memoria = [],
+  memoria = creacionMemoriaVacia,
   acumuladorA = 0,
   acumuladorB = 0,
   programCounter = 0,
   mensajeError = ""
 }
 
-nop microprocesador = microprocesador{programCounter = (programCounter microprocesador) + 1}
+siguienteInstruccion :: Int -> Int
+siguienteInstruccion pc = pc + 1
+
+
+nop :: Microprocesador -> Microprocesador
+nop micro = micro {programCounter = siguienteInstruccion (programCounter micro)}
+
+str :: Int -> Int -> Microprocesador -> Microprocesador
+str addr val micro | addr > 1024 = micro{mensajeError = "ERROR: Posicion de memoria no existente"}
+                   | otherwise = micro{memoria = take (addr-1) (memoria micro) ++ [val] ++ drop (addr) (memoria micro), programCounter = siguienteInstruccion (programCounter micro)}
