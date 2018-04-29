14c349b7677df2f7c9af61ce462f9a32c3274014
Maximiliano Feldman <maxifeldman@hotmail.com>
2018-04-28 22:31:19 -0300

    Corregidas instrucciones add, swap, divide. añadido tipo de dato Instruccion

--- MicroEntrega1.hs
+++ MicroEntrega1.hs
@@ -6,6 +6,8 @@ data Microprocesador = Microprocesador {
   mensajeError :: String
 } deriving Show
 
+type Instruccion = Microprocesador -> Microprocesador
+
 creacionMemoriaVacia :: [Int]
 creacionMemoriaVacia = replicate 1024 0
 
@@ -23,26 +25,26 @@ siguienteInstruccion pc = pc + 1
 intercambioValorDeLista :: [Int] -> Int -> Int -> [Int]
 intercambioValorDeLista memoria addr val = take (addr-1) memoria ++ [val] ++ drop addr memoria
 
-nop :: Microprocesador -> Microprocesador
+nop :: Instruccion
 nop micro = micro {programCounter = siguienteInstruccion (programCounter micro)}
 
-str :: Int -> Int -> Microprocesador -> Microprocesador
+str :: Int -> Int -> Instruccion
 str addr val micro | addr > 1024 = micro{mensajeError = "ERROR: Posicion de memoria no existente"}
                    | otherwise = micro{memoria = intercambioValorDeLista (memoria micro) addr val, programCounter = siguienteInstruccion (programCounter micro)}
 
-lod :: Int -> Microprocesador -> Microprocesador
+lod :: Int -> Instruccion
 lod addr micro | addr > 1024 = micro{mensajeError = "ERROR: Posicion de memoria no existente"}
                | otherwise = micro{acumuladorA = (memoria micro)!!(addr-1), programCounter = siguienteInstruccion (programCounter micro)}
 
-lodv :: Int -> Microprocesador -> Microprocesador
+lodv :: Int -> Instruccion
 lodv val micro = micro{acumuladorA = val, programCounter = siguienteInstruccion (programCounter micro)}
 
-add :: Microprocesador -> Microprocesador
-add micro = micro{acumuladorA = acumuladorA + acumuladorB, acumuladorB = 0, programCounter = siguienteInstruccion (programCounter micro)}
+add :: Instruccion
+add micro = micro{acumuladorA = (acumuladorA micro) + (acumuladorB micro), acumuladorB = 0, programCounter = siguienteInstruccion (programCounter micro)}
 
-swap :: Microprocesador -> Microprocesador
-swap micro = micro{acumuladorA = acumuladorB, acumuladorB = acumuladorA, programCounter = siguienteInstruccion (programCounter micro)}
+swap :: Instruccion
+swap micro = micro{acumuladorA = acumuladorB micro, acumuladorB = acumuladorA micro, programCounter = siguienteInstruccion (programCounter micro)}
 
 divide :: Microprocesador -> Microprocesador
-divide micro | (micro{acumuladorB})==0 = micro{mensajeError = "DIVISION BY ZERO”}
-             | otherwise = micro{acumuladorA = acumuladorA / acumuladorB, acumuladorB = 0, programCounter = siguienteInstruccion (programCounter micro)}
+divide micro | (acumuladorB micro)==0 = micro{mensajeError = "DIVISION BY ZERO"}
+             | otherwise = micro{acumuladorA = div (acumuladorA micro) (acumuladorB micro), acumuladorB = 0, programCounter = siguienteInstruccion (programCounter micro)}
