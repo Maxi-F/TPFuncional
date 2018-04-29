875349950bbb8d3a3f71eedc3fae0ca06e637428
Maximiliano Feldman <maxifeldman@hotmail.com>
2018-04-29 15:53:17 -0300

    Añadidos programaSumador y programaDivisionCero.

--- MicroEntrega1.hs
+++ MicroEntrega1.hs
@@ -7,12 +7,13 @@ data Microprocesador = Microprocesador {
 } deriving Show
 
 type Instruccion = Microprocesador -> Microprocesador
+type Programa = Microprocesador -> Microprocesador
 
-creacionMemoriaVacia :: [Int]
-creacionMemoriaVacia = replicate 1024 0
+creacionMemoriaVacia :: Int -> [Int]
+creacionMemoriaVacia espaciosVacios = replicate espaciosVacios 0
 
 xt8088 = Microprocesador {
-  memoria = creacionMemoriaVacia,
+  memoria = creacionMemoriaVacia 1024,
   acumuladorA = 0,
   acumuladorB = 0,
   programCounter = 0,
@@ -45,6 +46,12 @@ add micro = micro{acumuladorA = (acumuladorA micro) + (acumuladorB micro), acumu
 swap :: Instruccion
 swap micro = micro{acumuladorA = acumuladorB micro, acumuladorB = acumuladorA micro, programCounter = siguienteInstruccion (programCounter micro)}
 
-divide :: Microprocesador -> Microprocesador
-divide micro | (acumuladorB micro)==0 = micro{mensajeError = "DIVISION BY ZERO"}
+divide :: Instruccion
+divide micro | (acumuladorB micro)==0 = micro{mensajeError = "DIVISION BY ZERO", programCounter = siguienteInstruccion (programCounter micro)}
              | otherwise = micro{acumuladorA = div (acumuladorA micro) (acumuladorB micro), acumuladorB = 0, programCounter = siguienteInstruccion (programCounter micro)}
+
+programaSumador :: Programa
+programaSumador = add.(lodv 22).swap.(lodv 10)
+
+programaDivisionCero :: Programa
+programaDivisionCero = divide.(lod 1).swap.(lod 2).(str 2 0).(str 1 2)
