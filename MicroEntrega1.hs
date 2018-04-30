96f9e0ac77c7553fdf0c7bead4267e874f331909
Maximiliano Feldman <maxifeldman@hotmail.com>
2018-04-30 18:56:36 -0300

    Correccion de algunas cosas conceptuales

--- MicroEntrega1.hs
+++ MicroEntrega1.hs
@@ -1,3 +1,8 @@
+module MicroEntrega1 where
+
+creacionMemoriaVacia :: Int -> [Int]
+creacionMemoriaVacia espaciosVacios = replicate espaciosVacios 0
+
 data Microprocesador = Microprocesador {
   memoria :: [Int],
   acumuladorA :: Int,
@@ -9,11 +14,24 @@ data Microprocesador = Microprocesador {
 type Instruccion = Microprocesador -> Microprocesador
 type Programa = Microprocesador -> Microprocesador
 
-creacionMemoriaVacia :: Int -> [Int]
-creacionMemoriaVacia espaciosVacios = replicate espaciosVacios 0
-
 xt8088 = Microprocesador {
   memoria = creacionMemoriaVacia 1024,
+  acumuladorA = 0,
+  acumuladorB = 0,
+  programCounter = 0,
+  mensajeError = ""
+}
+
+fp20 = Microprocesador {
+  memoria = creacionMemoriaVacia 1024,
+  acumuladorA = 7,
+  acumuladorB = 24,
+  programCounter = 0,
+  mensajeError = ""
+}
+
+at8086 = Microprocesador {
+  memoria = [1..20] ++ (creacionMemoriaVacia 1004),
   acumuladorA = 0,
   acumuladorB = 0,
   programCounter = 0,
@@ -30,11 +48,11 @@ nop :: Instruccion
 nop micro = micro {programCounter = siguienteInstruccion (programCounter micro)}
 
 str :: Int -> Int -> Instruccion
-str addr val micro | addr > 1024 = micro{mensajeError = "ERROR: Posicion de memoria no existente"}
+str addr val micro | addr > 1024 = micro{mensajeError = "ERROR: Posicion de memoria no existente", programCounter = siguienteInstruccion (programCounter micro)}
                    | otherwise = micro{memoria = intercambioValorDeLista (memoria micro) addr val, programCounter = siguienteInstruccion (programCounter micro)}
 
 lod :: Int -> Instruccion
-lod addr micro | addr > 1024 = micro{mensajeError = "ERROR: Posicion de memoria no existente"}
+lod addr micro | addr > 1024 = micro{mensajeError = "ERROR: Posicion de memoria no existente", programCounter = siguienteInstruccion (programCounter micro)}
                | otherwise = micro{acumuladorA = (memoria micro)!!(addr-1), programCounter = siguienteInstruccion (programCounter micro)}
 
 lodv :: Int -> Instruccion
@@ -50,24 +68,8 @@ divide :: Instruccion
 divide micro | (acumuladorB micro)==0 = micro{mensajeError = "DIVISION BY ZERO", programCounter = siguienteInstruccion (programCounter micro)}
              | otherwise = micro{acumuladorA = div (acumuladorA micro) (acumuladorB micro), acumuladorB = 0, programCounter = siguienteInstruccion (programCounter micro)}
 
-fp20 = Microprocesador {
-  memoria = creacionMemoriaVacia 1024,
-  acumuladorA = 7,
-  acumuladorB = 24,
-  programCounter = 0,
-  mensajeError = ""
-}
-
 programaSumador :: Programa
 programaSumador = add.(lodv 22).swap.(lodv 10)
-
-at8086 = Microprocesador {
-  memoria = [1..20],
-  acumuladorA = 0,
-  acumuladorB = 0,
-  programCounter = 0,
-  mensajeError = ""
-}
 
 programaDivisionCero :: Programa
 programaDivisionCero = divide.(lod 1).swap.(lod 2).(str 2 0).(str 1 2)
