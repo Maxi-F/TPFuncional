94e6cc10e844cb6977b1ef60e2a482e0e38fae2b
Maximiliano Feldman <maxifeldman@hotmail.com>
2018-05-17 15:34:10 -0300

    Añadida serie de programas en micro. Añadida IFNZ

--- MicroEntrega1.hs
+++ MicroEntrega1.hs
@@ -1,29 +1,6 @@
---            ,,d00KKKKK0KKKKKKKKKKKKKKKKKKKKKk:,
---          ,,':OK0K0KK00KKKKKKKKKKKKKKKKKKKKK0Oxxxxxxddoollc:;
---       ;:clodO0KK00K00KKKKKKK0000OO0KKKKKKKKKKKKKKKKKKXKKKOxc
---  :cloxkO0KKKKKKKKK0Okkkxxxdddddddx0KKKKKKKKKKKKKK00Okxolc:,'
--- xO0KKKKKKKKKKKKKKKK0000OkOOOO0000KKKKKK0OOkkxxol::;,''..
--- 000KKKKKKKKKKKKKKKKK00000OOOkkkxxxxdddoollclll:'.
---  ::ccclllccccc:::;;;,,coolllllcclllllloooooolol:'.
---          .           .:oooollllllllloolllllclcll;
---         .            'cocclllll;,clccloolcclll;;:'.
---         .           .:oc:clooll:;llcclooolc:clcc:'
---          .         .;lllllccllllllllloloooll:clc,.
---          .  .';;;..;lllllollollllllloolloloolc;'.                PUNTITO MÁS POR PONER UN ASCII ART??
---          '..clccllcllllllloolooollllllllllolll;.
---          '.;ll::cloolooloooooooooooooooooolloll;.                      TUTOR USTED ES DIABÓLICO
---          '.,lolloolooloooooooooooooooooooooloooc,.
---           '.,cllcllooooloooooooooooooooolloooool;.
---                  ,colloololooooooooolooolccccc:;
---                 .'colooolloolooollooooooolllc:;,
---                 .'coloooooooollllllloooooooollollc;
---                 .'coolloool:.........',::;::cccccc;
---               .'';loollolll:...       .;'.
---              .':;:loooolllllllcc::;;;;:c;
---             ..:oc:cooloololllllllloool:,,
-
-
 module MicroEntrega1 where
+
+import Text.Show.Functions
 
 creacionMemoriaVacia :: Int -> [Int]
 creacionMemoriaVacia espaciosVacios = replicate espaciosVacios 0
@@ -33,7 +10,8 @@ data Microprocesador = Microprocesador {
   acumuladorA :: Int,
   acumuladorB :: Int,
   programCounter :: Int,
-  mensajeError :: String
+  mensajeError :: String,
+  programas :: [Programa]
 } deriving Show
 
 type Instruccion = Microprocesador -> Microprocesador
@@ -44,7 +22,8 @@ xt8088 = Microprocesador {
   acumuladorA = 0,
   acumuladorB = 0,
   programCounter = 0,
-  mensajeError = ""
+  mensajeError = "",
+  programas = []
 }
 
 fp20 = Microprocesador {
@@ -52,7 +31,8 @@ fp20 = Microprocesador {
   acumuladorA = 7,
   acumuladorB = 24,
   programCounter = 0,
-  mensajeError = ""
+  mensajeError = "",
+  programas = []
 }
 
 at8086 = Microprocesador {
@@ -60,24 +40,22 @@ at8086 = Microprocesador {
   acumuladorA = 0,
   acumuladorB = 0,
   programCounter = 0,
-  mensajeError = ""
+  mensajeError = "",
+  programas = []
 }
-
-siguienteInstruccion :: Int -> Int
-siguienteInstruccion pc = pc + 1
 
 intercambioValorDeLista :: [Int] -> Int -> Int -> [Int]
 intercambioValorDeLista memoria addr val = take (addr-1) memoria ++ [val] ++ drop addr memoria
 
 nop :: Instruccion
-nop micro = micro {programCounter = siguienteInstruccion (programCounter micro)}
+nop micro = micro {programCounter = (programCounter micro) + 1}
 
 str :: Int -> Int -> Instruccion
-str addr val micro | addr > 1024 = nop micro{mensajeError = "ERROR: Posicion de memoria no existente"}
+str addr val micro | addr > 1024 = micro{mensajeError = "ERROR: Posicion de memoria no existente"}
                    | otherwise = nop micro{memoria = intercambioValorDeLista (memoria micro) addr val}
 
 lod :: Int -> Instruccion
-lod addr micro | addr > 1024 = nop micro{mensajeError = "ERROR: Posicion de memoria no existente"}
+lod addr micro | addr > 1024 = micro{mensajeError = "ERROR: Posicion de memoria no existente"}
                | otherwise = nop micro{acumuladorA = (memoria micro)!!(addr-1)}
 
 lodv :: Int -> Instruccion
@@ -90,8 +68,18 @@ swap :: Instruccion
 swap micro = nop micro{acumuladorA = acumuladorB micro, acumuladorB = acumuladorA micro}
 
 divide :: Instruccion
-divide micro | (acumuladorB micro)==0 = nop micro{mensajeError = "DIVISION BY ZERO"}
+divide micro | (acumuladorB micro)==0 = micro{mensajeError = "DIVISION BY ZERO"}
              | otherwise = nop micro{acumuladorA = div (acumuladorA micro) (acumuladorB micro), acumuladorB = 0}
+
+puntoDeCorteIFNZ :: [Instruccion] -> Microprocesador -> Bool
+puntoDeCorteIFNZ cola micro =  (length cola) == 0 || ((mensajeError micro) /= []) || (acumuladorA micro) == 0
+
+ifnz :: [Instruccion] -> Instruccion
+ifnz (instruccion:cola) micro | puntoDeCorteIFNZ cola micro = micro
+                              | otherwise = ifnz cola (instruccion micro)
+
+cargarProgramas :: [Instruccion] -> Programa
+cargarProgramas instrucciones micro = micro{programas = (programas micro) ++ instrucciones}
 
 programaSumador :: Programa
 programaSumador = add.(lodv 22).swap.(lodv 10)
